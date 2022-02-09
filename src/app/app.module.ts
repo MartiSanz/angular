73a82501beb79000ef3753ccee6092c3a6fc8abb
MartiSanz <msanzcaparros@gmail.com>
2022-02-09 14:59:28 -0300

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 /* El generador agregó automáticamente ProductAlertsComponent para que esté disponible para otros componentes en la aplicación. */
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -19,6 +20,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
       { path: '', component: ProductListComponent },
       /* agrego una ruta para los detalles del producto*/
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ])
   ],
   declarations: [
@@ -26,7 +28,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   bootstrap: [
     AppComponent
