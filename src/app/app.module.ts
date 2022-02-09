27f6a38557e163f8451edcd6bfb76a5bceba0418
MartiSanz <msanzcaparros@gmail.com>
2022-02-09 16:10:39 -0300

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,26 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 /* El generador agregó automáticamente ProductAlertsComponent para que esté disponible para otros componentes en la aplicación. */
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       /* agrego una ruta para los detalles del producto*/
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
-    ])
+      { path: 'shipping', component: ShippingComponent },
+    ]),
   ],
   declarations: [
     AppComponent,
@@ -29,16 +33,12 @@ import { CartComponent } from './cart/cart.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent,
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent],
 })
-
-
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 // importo la interfac products
 import { Product } from './products';
+// precio de envios
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +11,7 @@ export class CartService {
   // creo una variable que almacene la matriz de productos
   items: Product[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   //agrega un producto al carrito
   addToCart(product: Product) {
@@ -25,5 +27,13 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  // get precio de los envios
+  // type: string; price: number --> son los datos que contiene el JSON de envios
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 }
